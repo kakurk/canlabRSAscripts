6fd68ede72d7cbbf22c87ef76f651aeff3a818cb
KYLE ANDREW KURKELA <kak53@hammer6.rcc.psu.edu>
2017-03-21 19:38:19 -0400

    Added documentation; write stats to a file

--- run_splithalf_correlations_FAMEret8RSAmodel.m
+++ run_splithalf_correlations_FAMEret8RSAmodel.m
@@ -14,9 +14,9 @@ else % if not on unix, assume we are on Anvil
 end
 
 %% Set analysis parameters
-subjects   = {'18y404'  '18y566'  '20y297' '20y415'  '20y439'}; % '20y396'
-rois       = {'rLTG_left'}; %add in leftHC and right HC for starters
-study_path = '/gpfs/group/n/nad12/RSA/Analysis_ret/FAMEret8RSA_hrf';
+subjects   = {'18y404'  '18y566'  '20y297' '20y415'  '20y439'}; % '20y396' <-- this subjects doesn't have a model run
+rois       = {'rLTG_left'}; %
+study_path = '/gpfs/group/n/nad12/RSA/Analysis_ret/FAMEret8RSA_hrf'; % path on hammer
 
 % initalizing the sum of weighted zs all array
 z_all   = cell(1,length(rois));
@@ -25,7 +25,7 @@ rho_all = cell(1,length(rois));
 for ss = 1:length(subjects)
  
     % Edit the SPM.mat file to use paths here on Hammer
-    if isunix
+    if isunix % only execute if we are on a Unix system (i.e., Hammer)
         spm_changepath(fullfile(study_path, subjects{ss}, 'SPM.mat'), 'S:\nad12\FAME8', '/gpfs/group/n/nad12/RSA')
         spm_changepath(fullfile(study_path, subjects{ss}, 'SPM.mat'), '\', '/')
     end
@@ -43,7 +43,7 @@ for ss = 1:length(subjects)
 
     roi_label = rois{rr}; % name of ROI mask used for running correlations  
 
-    % the averaged betas
+    % path to the averaged betas
     HREC     = fullfile(data_path, 'average_beta_HREC.nii');
     HFAM     = fullfile(data_path, 'average_beta_HFAM.nii');
     FAREC    = fullfile(data_path, 'average_beta_FAREC.nii');
@@ -53,44 +53,40 @@ for ss = 1:length(subjects)
 
     % load two halves as CoSMoMVPA dataset structs.
     % Chunks = Runs  Targets = trial type conditions
-    HREC_ds  = cosmo_fmri_dataset(HREC,'mask',mask_fn,... %encoding... run 1 (chunk) Target(rem) will be identified as 1
-                                         'targets',(1)',... %ex: Rem
-                                         'chunks',(1)); %ex: encoding (avg. of all enc runs)
+    HREC_ds  = cosmo_fmri_dataset(HREC,'mask',mask_fn,...
+                                         'targets',(1)',... % HREC
+                                         'chunks',(1)); % (avg. of all runs)
 
     HFAM_ds  = cosmo_fmri_dataset(HFAM,'mask',mask_fn,...
-                                         'targets',(2)',... %ex. Know
-                                         'chunks',(1)); %encoding (avg. of all enc runs)
+                                         'targets',(2)',... % HFAM
+                                         'chunks',(1)); % (avg. of all runs)
 
     FAREC_ds = cosmo_fmri_dataset(FAREC,'mask',mask_fn,...
-                                         'targets',(3)',... %New
-                                         'chunks',(1)); %ret
+                                         'targets',(3)',... % FAREC
+                                         'chunks',(1)); % (avg. of all runs)
                                      
     FAFAM_ds = cosmo_fmri_dataset(FAFAM,'mask',mask_fn,...
-                                         'targets',(4)',... %New
-                                         'chunks',(1)); %ret
+                                         'targets',(4)',... % FAFAM
+                                         'chunks',(1)); % (avg. of all runs)
    
-    % Combine files at encoding and retrieval to create two files (i.e.,
-    % stacking)
+    % Stack above files
     % make sure all ds_* changed from here on
     ds_all   = cosmo_stack({HREC_ds, HFAM_ds, FAREC_ds, FAFAM_ds});
 
-    % Data set labels
+    % Data set labels (same order as above)
     ds_all.sa.labels = {'HREC';'HFAM';'FAREC';'FAFAM'}; 
     
-    % cosmo fxn to make sure data in right format
+    % cosmo checks to make sure data in right format
     cosmo_check_dataset(ds_all);
 
-    % change if you change ds_* above
-    nClasses = numel(ds_all.sa.labels);
-
-    % get the sample data - samples are the correlations being ran on each
-    % voxel (a bunch of numbers)
+    % get the samples. The samples are the beta values from all of the
+    % vovels in the current ROI
     all_ds_samples = ds_all.samples;
 
-    % compute all correlation values between the two halves, resulting
-    % in a 6x6 matrix. Store this matrix in a variable 'rho'.
+    % compute all correlation values between the trial types, resulting
+    % in a nTrialTypes x nTrialTypes matrix. Store this matrix in a variable 'rho'.
     % Hint: use cosmo_corr (or builtin corr, if the matlab stats toolbox
-    %       is available) after transposing the samples in the two halves.
+    %       is available) after transposing the samples.
     % >@@>
     rho = cosmo_corr(all_ds_samples');
     % <@@<
@@ -104,13 +100,15 @@ for ss = 1:length(subjects)
     % <@@<
 
     %% store and save results
+    % path to save results
     output_path = fullfile(study_path, subjects{ss}, 'RSA_Results');
 
+    % create the output path if it doesn't already exist
     if ~exist(output_path, 'dir')
         mkdir(output_path)
     end
     
-    %% Write r matrix to Excel
+    %% Write rho matrix to Excel
     filename = ['RSAtest_', subjects{ss}, '_' roi_label '_rho_.xlsx'];
     rho %#ok<*NOPTS>
     xlswrite(fullfile(output_path, filename), rho)
@@ -122,9 +120,8 @@ for ss = 1:length(subjects)
 
     %% Store result in a structure for later calculations
 
-    % store the result for this subject in z_all
-    % (at the i_subj-th position), so that
-    % group statistics can be computed
+    % store the result for this subject in z_all and rho_all cell arrays 
+    % so that group statistics can be computed
     % >@@>
     z_all{rr}   = cat(3, z_all{rr}, z);
     rho_all{rr} = cat(3, rho_all{rr}, rho);
@@ -136,22 +133,47 @@ for ss = 1:length(subjects)
 end
 
 %% Compute T-Statistics
+% Do a t-test on the average z-value for each combination of conditions to
+% see if the z-value is signifcantly greater than zero (which would
+% indicate no correlation)
 
+% initalize an empty cell array to hold the stats matrices
 stats_all = cell(1,length(rois));
 
+% output path
+outpath = fullfile(study_path, 'RSA_group_results')
+
+% create the output path if it doesn't already exist
+if ~exist(outpath, 'dir')
+    mkdir(outpath)
+end
+
+% for each roi..
 for rr = 1:length(rois)
     
+    % how many rows and columns are there in the z matrix?
     [rows, cols, ~] = size(z_all{rr});
     
+    % for each row for each column
     for crow = 1:rows
         for ccol = 1:cols
+            
+            % do a one-sample t-test across subjects to see if the average
+            % z value is signficnatly greater than 0 for this pair of
+            % conditions in this ROI
             [h,p,ci,stats]            = ttest(z_all{rr}(crow,ccol,:));
             stats_all{rr}(crow, ccol) = stats.tstat;
+            
         end
     end
     
+    % Display the statistics matrix in the command window
     fprintf('T Statistics = \n\n')
     disp(stats_all{rr})
+    
+    % Save the t statistics to a file
+    filename = [roi_label '_results.xlsx']
+    xlswrite(fullfile(outpath, filename), stats_all{rr})
     
 end
 
@@ -192,7 +214,6 @@ for rr = 1:length(rois)
     desc=sprintf(['Average splithalf correlation across subjects '...
                     'in mask ''%s'''], rois{rr});
     title(desc)
-
 
     col_limits(rr,:) = get(gca, 'clim');
 end
